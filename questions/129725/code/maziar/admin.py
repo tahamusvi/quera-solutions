0e73008f8c5238af02238c70fc7222008800dbdb
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-06-29 13:56:12 -0700

    129725 is updated by maziar,done

--- questions/129725/code/maziar/admin.py
+++ questions/129725/code/maziar/admin.py
@@ -8,6 +8,9 @@ from django.contrib import admin
 class CustomUser_admin(admin.ModelAdmin):
    list_display = ['username','first_name','last_name','gender','national_code','birthday_date' ]
    list_filter=['username','full_name']
+   search_fields = ['username', 'full_name']
+   ordering = ('ceremony_datetime',)
+
    
    def first_name(self, obj):
         return obj.full_name.split()[0]
--- questions/129725/code/maziar/forms.py
+++ questions/129725/code/maziar/forms.py
@@ -1,7 +1,49 @@
 from django import forms
 from .models import CustomUser
 import re
+from django_jalali.forms import widgets
 
+
+
+class CustomUserForm(forms.ModelForm):
+   # username = forms.CharField(max_length=256)
+   # full_name = forms.CharField(max_length=256)
+   # gender = forms.ChoiceField(choices=CustomUser.GENDER_CHOICES)
+    national_code = forms.CharField(max_length=10,min_length=10)
+   # birthday_date = widgets.jDateInput()
+   # ceremony_datetime = widgets.jDateTimeInput()
+  #  country = forms.CharField(max_length=100, initial='Iran')
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data['full_name']
+        parts = full_name.split()
+        if len(parts) < 2:
+            raise forms.ValidationError("Full name must contain at least first and last name.")
+        
+        first_name = parts[0].capitalize()
+        last_name = ' '.join([part.capitalize() for part in parts[1:]])
+        
+        if full_name != f"{first_name} {last_name}":
+            raise forms.ValidationError("Full name must be in title case format.")
+        
+        return f"{first_name} {last_name}"
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        #['username', 'full_name', 'gender', 'national_code', 'birthday_date', 'ceremony_datetime', 'country']
+
+
+
+
+
+
+
+
+
+
+
+"""
 class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
@@ -24,3 +66,4 @@ class CustomUserForm(forms.ModelForm):
 
             return full_name
         
+"""
